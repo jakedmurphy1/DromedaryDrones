a4d81a70cf53346e3fbbf765136be864fd35c440
ty-d <tpgd1373@gmail.com>
2020-03-16 17:05:54 -0400

    Minor additions/renaming files

--- src/dronesimulation/TravellingSalesman.java
+++ src/dronesimulation/TravellingSalesman.java
@@ -4,7 +4,13 @@ import java.util.HashSet;
 
 public class TravellingSalesman {
     public static DeliveryPoint[] travellingSalesman(HashSet<DeliveryPoint> points) {
+        // TODO: complete this algorithm
         DeliveryPoint[] deliveryPoints = points.toArray(new DeliveryPoint[points.size()]);
         return deliveryPoints;
+    }
+
+    // Recursive function that traverses the tree
+    public static void tspRecursive() {
+
     }
 }
