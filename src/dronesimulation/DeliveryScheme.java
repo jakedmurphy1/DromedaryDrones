381672ac45eb0219ce379fbf467ca000536d652d
ty-d <tpgd1373@gmail.com>
2020-04-02 13:36:10 -0400

    Removed merge markers.

--- src/dronesimulation/DeliveryScheme.java
+++ src/dronesimulation/DeliveryScheme.java
@@ -5,19 +5,8 @@ import java.util.Queue;
 
 public interface DeliveryScheme {
 	//Fills drone with meals until it gets too close to weight capacity
-<<<<<<< HEAD
-<<<<<<< HEAD
 	public double fillDrone(final Drone drone);
-=======
-	public List<DeliveryPoint> fillDrone(double weightCap);
->>>>>>> continued working in Simulation
-=======
-	public List<DeliveryPoint> fillDrone(double weightCap);
-=======
-	public double fillDrone(final Drone drone);
->>>>>>> Simulation now runs without errors. Needs more testing.
->>>>>>> Simulation now runs without errors. Needs more testing.
-	
+
 	public void addOrder(Order order);
 
 	public boolean isEmpty();
